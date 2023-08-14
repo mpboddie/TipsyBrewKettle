1f1eb4f761b46940ff4158eb7d061380494639fc
Mark Boddie <mpboddie@gmail.com>
2023-08-13 21:23:10 -0400

    Removed old clunky SendHTML

--- src/htmlData.h
+++ src/htmlData.h
@@ -92,6 +92,33 @@ const char htmlMain[] PROGMEM = R"rawliteral(
     <a id="tbkVersion" href="version">TBK version </a>
     <div id="heap" style="display: none;"></div>
     <script>
+        var gateway = `ws://${window.location.hostname}/ws`;
+        var websocket;
+        function initWebSocket() {
+            console.log('Trying to open a WebSocket connection...');
+            websocket = new WebSocket(gateway);
+            websocket.onopen    = onOpen;
+            websocket.onclose   = onClose;
+            websocket.onmessage = onMessage;
+        }
+        function onOpen(event) {
+            console.log('Connection opened');
+        }
+
+        function onClose(event) {
+            console.log('Connection closed');
+            setTimeout(initWebSocket, 2000);
+        }
+        function onMessage(event) {
+            var state;
+            if (event.data == "1"){
+            state = "ON";
+            }
+            else{
+            state = "OFF";
+            }
+            document.getElementById('state').innerHTML = state;
+        }
         $(document).ready(function(){
             $("#currentTemp").html(status.tempreading + "&#176;C");
             if(status.pump) {
